896f56e826033aac43ba77c633d426a36a8689bf
Jesus Moris <jesus@soluciontotal.cl>
2023-01-15 01:32:32 -0300

    prueba

--- backend/app/Http/Controllers/ProyectoController.php
+++ backend/app/Http/Controllers/ProyectoController.php
@@ -266,7 +266,7 @@ class ProyectoController extends Controller
             }
             $start_sha = $filtered[0]['sha'];
             $end_sha = $filtered[count($filtered)-1]['sha'];
-            $str = 'cd '.$path_project.' &&  /usr/bin/python2.7 ../scripts/git_complexity_trend.py --end '.$start_sha.' --start '.$end_sha.' --file '.$entity;
+            $str = 'cd '.$path_project.' &&  /usr/bin//usr/bin/python2 ../scripts/git_complexity_trend.py --end '.$start_sha.' --start '.$end_sha.' --file '.$entity;
             Log::info($str);
             $res = shell_exec($str);
             $lines = explode(PHP_EOL, $res);
@@ -402,9 +402,9 @@ class ProyectoController extends Controller
         }
         $graficos = [];
         foreach($collabs as $collab){
-            $grafico1 = shell_exec('cd '.$path.' && python2.7 grafico_usuario.py  \''.json_encode($data[$collab['name']]). '\'');
+            $grafico1 = shell_exec('cd '.$path.' && /usr/bin/python2 grafico_usuario.py  \''.json_encode($data[$collab['name']]). '\'');
             Log::info($grafico1);
-            $grafico2 = shell_exec('cd '.$path.' && python2.7 grafico_usuario_commits.py  \''.json_encode($data[$collab['name']]). '\'');
+            $grafico2 = shell_exec('cd '.$path.' && /usr/bin/python2 grafico_usuario_commits.py  \''.json_encode($data[$collab['name']]). '\'');
             $graficos[$collab['name']] = [
                 'activity' => $grafico1,
                 'commits' => $grafico2
@@ -426,7 +426,7 @@ class ProyectoController extends Controller
         $entity1 = $this->getFileRangeComplexityAnalysis($request->project_id, $maat[0]->entity, $repo->created_at);
         $entity2 = $this->getFileRangeComplexityAnalysis($request->project_id, $maat[1]->entity, $repo->created_at);
         $entity3 = $this->getFileRangeComplexityAnalysis($request->project_id, $maat[2]->entity, $repo->created_at);
-        $str = 'cd '.$path.' && python2.7 grafico.py ';
+        $str = 'cd '.$path.' && /usr/bin/python2 grafico.py ';
         $grafico1 = shell_exec($str.implode(',',$entity1['x']).' '.implode(',',$entity1['y']));
         $grafico2 = shell_exec($str.implode(',',$entity2['x']).' '.implode(',',$entity2['y']));
         $grafico3 = shell_exec($str.implode(',',$entity3['x']).' '.implode(',',$entity3['y']));
@@ -625,8 +625,8 @@ class ProyectoController extends Controller
         }
         $graficos = [];
         foreach($collabs as $collab){
-            $grafico1 = shell_exec('cd '.$path.' && python2.7 grafico_usuario.py  \''.json_encode($data[$collab->pivot->gh_user]). '\'');
-            $grafico2 = shell_exec('cd '.$path.' && python2.7 grafico_usuario_commits.py  \''.json_encode($data[$collab->pivot->gh_user]). '\'');
+            $grafico1 = shell_exec('cd '.$path.' && /usr/bin/python2 grafico_usuario.py  \''.json_encode($data[$collab->pivot->gh_user]). '\'');
+            $grafico2 = shell_exec('cd '.$path.' && /usr/bin/python2 grafico_usuario_commits.py  \''.json_encode($data[$collab->pivot->gh_user]). '\'');
             $graficos[$collab->pivot->gh_user] = [
                 'activity' => $grafico1,
                 'commits' => $grafico2
